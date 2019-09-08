09c3d36ffb4bb891eededc5b2296be3f33ea34e8
stonefollari <stonefollari1998@gmail.com>
2019-09-08 03:50:13 -0400

    Adding server config files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+echo "Hey all, trying to get all the server config working. The source code should be on github.";
+echo '</br>';
+echo "Clone the repo and start locally developing some stuff. Send your github usernames to me, and your gmails so I can add you to the server.";
+echo '</br>';
+echo "I created the mySQL on the server but am testing how to access it within the php currently.";
+
+
+
+?>
