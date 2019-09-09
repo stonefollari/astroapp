524ce98ad17840f4b2386b2771707b5b4bf85ec5
stonefollari <stonefollari1998@gmail.com>
2019-09-09 02:33:22 -0400

    Updating server config files

--- web/index.php
+++ web/index.php
@@ -5,8 +5,10 @@ echo "Hey all, trying to get all the server config working. The source code shou
 echo '</br>';
 echo "Clone the repo and start locally developing some stuff. Send your github usernames to me, and your gmails so I can add you to the server.";
 echo '</br>';
-echo "I created the mySQL on the server but am testing how to access it within the php currently.";
+echo "I created the mySQL db on the server but am testing how to access it within the php currently.";
 
+
+$servername = "mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>"
 
 
 ?>
