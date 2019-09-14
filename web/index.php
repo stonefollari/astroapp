426dd17bb8c0ef7a930ace6d4a4a3f41a9419a63
Gabriel Costa <gabrielpatrial@gmail.com>
2019-09-14 14:33:27 -0400

    This is a test

--- web/index.php
+++ web/index.php
@@ -1,14 +1,31 @@
-<?php
-
-
-echo "Hey all, trying to get all the server config working. The source code should be on github.";
-echo '</br>';
-echo "Clone the repo and start locally developing some stuff. Send your github usernames to me, and your gmails so I can add you to the server.";
-echo '</br>';
-echo "I created the mySQL db on the server but am testing how to access it within the php currently.";
-
-
-$servername = "mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>"
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+	<link rel="stylesheet" type "text/css" href="css/style.css">
+</head>
+<body>
+	<div>
+	<img src="img/logo.png" class="logo" alt "astro-logo">
+	</div>
+	<form action="main.php" class="login-form">
+		<div>
+			<label>Username:</label>
+			<input type = "text" name= "username" placeholder="Username">	
+		</div>
+		<div>
+			<label>Password:</label>
+			<input type = "password" name= "password" placeholder="Password">
+		</div>
+		<div>
+		<input onclick="index.php" type="submit" value="Login" class="login-button">
+		</div>
+	</form>
+	
+	<form action="createAccount.php" class="login-form">
+		<div>
+			<input onclick = "createAccount.php" type="submit" class="createAccount-button" value="Create Account">
+		</div>
+	</form>
+</body>
+</html>
