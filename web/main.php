f25e6bb72acbbd5f39d8132dc7394bc6edffadfd
Gabriel Costa <gabrielpatrial@gmail.com>
2019-09-17 22:30:57 -0400

    This is trial for CSS grid

--- web/main.php
+++ web/main.php
@@ -5,5 +5,12 @@
 </head>
 <body>
 
+ <div class="grid-container">
+  <div class="grid-item">1 Some text here.</div>
+  <div class="grid-item">2 Some text here.</div>
+  <div class="grid-item">3 Some text here.</div>
+  <div class="grid-item">4 Some text here.</div>
+</div> 
+
 </body>
 </html>
