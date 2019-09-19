90c7f43eabd9904e27a4d2e4c5c8c5da669759ec
Francis Perez <45206888+francispz@users.noreply.github.com>
2019-09-18 21:24:56 -0400

    Update Users.php

--- web/protected/private/repositories/Users.php
+++ web/protected/private/repositories/Users.php
@@ -1,13 +1,17 @@
 <?php namespace AstroApp.Reprositories
 
-class Users{
+  class Users{
 
-  public static function get(String $_emailOfUserToGet){
-    return new User();
+    public static function get(String $_emailOfUserToGet){
+      return new User();
+    }
+
+    public static function insert(User $_userToInsert){
+      //save item to db
+    }
+
+    public static function update(User $_userToUpdate){
+      //save item to db
+    }
   }
-  
-  public static function insert(User $_userToInsert){
-    //save item to db
-  }
-  
-}
+?>
