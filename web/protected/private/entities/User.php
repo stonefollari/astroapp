ae7e2f0f8925061a6bac2caa04c43ffaf59f35dd
Francis Perez <45206888+francispz@users.noreply.github.com>
2019-09-18 21:43:43 -0400

    Create User.php

---
+++ web/protected/private/entities/User.php
@@ -0,0 +1,23 @@
+<?php namespace AstroApp.Entities
+    class User {
+        private int $userId;
+        private String $name;
+        private String $emailAddress;
+        private String $password;
+        
+        function __construct(String $_name = "") {
+            $this->name = $_name;
+        }
+        
+        //====Getters=====
+        public getName() {
+            return $this->$Name;
+        }
+        
+        //===Setters=======
+        public setName(String $_emailAddress) {
+            this $this->emailAddress = $_emailAddress;
+        }
+    }
+
+?>
