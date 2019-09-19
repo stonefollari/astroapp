ea5395861ebf1ebfc1c87a71d1d23c799b0465d3
Francis Perez <45206888+francispz@users.noreply.github.com>
2019-09-18 22:35:40 -0400

    Update User.php

--- web/protected/private/entities/User.php
+++ web/protected/private/entities/User.php
@@ -1,25 +1,23 @@
 <?php 
- namespace AstroApp\Web\Protected\Private\Entities {
+namespace AstroApp\Web\Protected\Private\Entities;
 
-    class User {
-        private int $userId;
-        private String $name;
-        private String $emailAddress;
-        private String $password;
-        
-        function __construct(String $_name = "") {
-            $this->name = $_name;
-        }
-        
-        //====Getters=====
-        public getName() {
-            return $this->$name;
-        }
-        
-        //===Setters=======
-        public setName(String $_emailAddress) {
-            this $this->emailAddress = $_emailAddress;
-        }
+class User {
+    private int $userId;
+    private String $name;
+    private String $emailAddress;
+    private String $password;
+
+    function __construct($_name) {
+        $this->name = $_name;
     }
- }
-?>
+
+    //====Getters=====
+    public getName() {
+        return $this->$name;
+    }
+
+    //===Setters=======
+    public setName($_emailAddress) {
+        this $this->emailAddress = $_emailAddress;
+    }
+}
