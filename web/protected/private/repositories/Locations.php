11987b0416cecef8073d94f064b33edea620654a
Francis Perez <45206888+francispz@users.noreply.github.com>
2019-09-18 22:38:48 -0400

    Update Locations.php

--- web/protected/private/repositories/Locations.php
+++ web/protected/private/repositories/Locations.php
@@ -1,10 +1,10 @@
 <?php 
-namespace AstroApp\Web\Protected\Private\Reprositories {
+namespace AstroApp\Web\Protected\Private\Reprositories;
 
-  class Locations{
-    public static function getLocationsByCountry($_country){
-      return [];
-    }
+use AstroApp\Web\Protected\Private\Entities;
+
+class Locations{
+  public static function getLocationsByCountry($_country){
+    return [];
   }
 }
-?>
