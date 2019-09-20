2b1e08e22716c50e61df9fb328fd089d588d5ab5
Gabriel Costa <gabrielpatrial@gmail.com>
2019-09-20 10:58:29 -0400

    Nothing to see here

--- web/main.php
+++ web/main.php
@@ -5,12 +5,6 @@
 </head>
 <body>
 
- <div class="grid-container">
-  <div class="grid-item">1 Some text here.</div>
-  <div class="grid-item">2 Some text here.</div>
-  <div class="grid-item">3 Some text here.</div>
-  <div class="grid-item">4 Some text here.</div>
-</div> 
 
 </body>
 </html>
