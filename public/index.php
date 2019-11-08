954f284b7149777d3f483e9dbdef6bdc79e97532
stonefollari <stonefollari1998@gmail.com>
2019-11-07 19:25:49 -0500

    Fixing index.php for real

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
-<?php
-    header('Location: ../Views/login.php');
-    exit;
-?>
+ <?php
+    define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+    define('APP', ROOT . 'src' . DIRECTORY_SEPARATOR);
+    define('VIEW', ROOT . 'src' . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR);
+    define('MODEL', ROOT . 'src' . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR);
+    define('DATA', ROOT . 'src' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR);
+    define('CORE', ROOT . 'src' . DIRECTORY_SEPARATOR . 'core'. DIRECTORY_SEPARATOR);
+    define('CONTROLLER', ROOT . 'src' . DIRECTORY_SEPARATOR . 'controller'. DIRECTORY_SEPARATOR);
+    $modules = [ROOT,APP,CORE,CONTROLLER,DATA];
+    
+    set_include_path(get_include_path() . PATH_SEPARATOR . implode(PATH_SEPARATOR, $modules));
+    spl_autoload_register('spl_autoload', false);
+    
+    new Application();
